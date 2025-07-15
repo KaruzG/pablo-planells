1ac0fd1ab505b74fb723937adc5b02a33deb88c3
Miguel Gresa <josemiguelcpcs@gmail.com>
2025-07-15 14:47:59 +0200

    feat: Added timeline columns

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,7 +1,34 @@
+import TimelineBox from "./TimelineBox";
+
+const sampleTimelineEvent = { date: "1/05/2024", title: "TFG", description: "Descripción del evento", image: "" }
+
 const Timeline = () => {
   return (
-    <section className="flex relative items-center justify-center">
-      <h1>Mi historia</h1>
+    <section className="flex flex-col relative items-center justify-center mb-10 px-3 ">
+      <h1 className="text-4xl font-bold">Mi historia</h1>
+      <ol className="grid grid-cols-[1fr_100px_1fr] mt-8 relative">
+        <li> { /* for testing purposes */}
+        </li>
+        <li className="mx-auto inset-0 border-l-1 border-r-1 relative timeline-horizontal-right-after timeline-horizontal-dot">
+        </li>
+        <li>
+          <TimelineBox timelineEvent={sampleTimelineEvent} />
+        </li>
+        <li>
+          <TimelineBox timelineEvent={sampleTimelineEvent} />
+        </li>
+        <li className="mx-auto inset-0 border-l-2 relative timeline-horizontal-left-after timeline-horizontal-dot">
+        </li>
+        <li>
+        </li>
+        <li>
+        </li>
+        <li className="mx-auto inset-0 border-l-1 border-r-1 relative timeline-horizontal-right-after timeline-horizontal-dot">
+        </li>
+        <li>
+          <TimelineBox timelineEvent={sampleTimelineEvent} />
+        </li>
+      </ol>
     </section>
   );
 };
