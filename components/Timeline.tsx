230ea35ba6af0933c2a46e59c782bd3efbac1e4b
Miguel Gresa <josemiguelcpcs@gmail.com>
2025-07-20 15:40:03 +0200

    feat(locales): Added locales for Nav and Timeline components

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import TimelineBox from "./TimelineBox";
 import TimelineLine from "./TimelineLine";
+import { useTranslations } from "next-intl";
 
 const sampleTimelineEvent = { date: "1/05/2024", title: "TFG", description: "Descripción del evento", image: "" }
 const sampleTimelineEvent1 = { date: "1/05/2024", title: "TFG", description: "Descripción del evento", image: "" }
@@ -8,9 +9,11 @@ const sampleTimelineEvent2 = { date: "1/05/2024", title: "TFG", description: "De
 const timelineEvents = [sampleTimelineEvent, sampleTimelineEvent1, sampleTimelineEvent2];
 
 const Timeline = () => {
+  const t = useTranslations('timeline');
+  
   return (
     <section className="flex flex-col relative items-center justify-center mb-10 px-3 ">
-      <h1 className="text-4xl font-bold">Mi historia</h1>
+      <h1 className="text-4xl font-bold">{t('title')}</h1>
       <ol className="grid grid-cols-[1fr_100px_1fr] mt-8 relative">
         {timelineEvents.map((event, index) => {
           if (index % 2 === 0) {
