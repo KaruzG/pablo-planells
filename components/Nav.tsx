6f407a4c1460efe978bc2dcb74b805da4e2aca82
Miguel Gresa <josemiguelcpcs@gmail.com>
2025-07-28 18:57:51 +0200

    fix(nav): Removed fixed nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,7 +5,7 @@ import {useTranslations} from 'next-intl';
 import { rubikBubbles } from "@/fonts/fonts"; 
 
 const styles = {
-    nav: `bg-white-gradient fixed top-0 left-0 w-full z-50`,
+    nav: `bg-white-gradient absolute top-0 left-0 w-full z-50`,
     list: "flex flex-row justify-around p-4 uppercase max-w-5xl mx-auto pt-6",
     listItem: `hover:font-bold hover:scale-105 transition-all duration-200 ${rubikBubbles.className} text-xl md:text-2xl text-white text-shadow-md`,
 };
