b9286aab4fa14c364c44642293d78342aaa94374
Miguel Gresa <josemiguelcpcs@gmail.com>
2025-07-29 13:40:36 +0200

    feat(hero): V2 design background and gradient transition

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,9 @@
 const Hero = () => {
   return (
-    <section className="flex relative items-center justify-center h-screen bg-[#7abfff]">
+    <section className={`flex relative items-center justify-center h-screen bg-size-[auto_600px] md:bg-size-[auto_1100px] bg-top-left md:bg-center bg-[url('/images/cloudBG.webp')]`}>
       <h1>Hero Component</h1>
       <p className="absolute bottom-20">V</p>
+      <div className="absolute bg-white-gradient-inverse bottom-0 w-screen h-8" />
     </section>
   );
 };
