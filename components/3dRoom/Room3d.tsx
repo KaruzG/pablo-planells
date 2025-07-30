1593da7bde6186c3285d66334a9461bea286f750
Miguel Gresa <josemiguelcpcs@gmail.com>
2025-07-30 12:30:03 +0200

    feat(Room): Better import, better view, added types

--- components/3dRoom/Room3d.tsx
+++ components/3dRoom/Room3d.tsx
@@ -1,17 +1,15 @@
 "use client"
-import { useLoader } from '@react-three/fiber'
 import { Canvas } from '@react-three/fiber';
-import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 import { Orbit } from './Orbit';
+import { Room } from './Room';
 
 export const Room3d = () => {
-    const gltf = useLoader(GLTFLoader, '/models/room.glb')
     return (
         <>
          <Canvas>
             <ambientLight intensity={1.5}  />
             <pointLight position={[10, 10, 10]} />
-            <primitive object={gltf.scene} scale={0.6} rotation={[0, Math.PI / 2 + 3.9, 0]} />
+            <Room />
             <Orbit />
          </Canvas>
         </>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import { Room3d } from "./3dRoom/Room3d";
 
 const Hero = () => {
   return (
     <section className={`flex relative items-center justify-center h-screen bg-size-[auto_600px] md:bg-size-[auto_1100px] bg-top-left md:bg-center bg-[url('/images/cloudBG.webp')]`}>
-      <Room3d />
+      <Suspense fallback={<div className="w-full h-full flex items-center justify-center">Loading...</div>}>
+        <Room3d />
+      </Suspense>
       <div className="absolute bg-white-gradient-inverse bottom-0 w-screen h-8" />
     </section>
   );
