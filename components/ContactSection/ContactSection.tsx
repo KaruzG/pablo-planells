6a1d46e16c90bd0c00c31168bdd208ce6074f2dd
KaruzG <josemiguelcpcs@gmail.com>
2025-08-07 11:24:14 +0200

    feat: Contact section

---
+++ components/ContactSection/ContactSection.tsx
@@ -0,0 +1,12 @@
+export const ContactSection = () => {
+    return (
+        <section className={`flex relative items-center justify-center h-screen bg-size-[auto_600px] md:bg-size-[auto_1100px] bg-top-left md:bg-center bg-[url('/images/cloudBG.webp')]`}>
+            <div className={`flex flex-row items-center justify-center min-w-1/2 h-1/2 m-19 bg-white rounded-lg shadow-lg`}>
+                <div>form</div>
+                <div>
+                    // Pon la imagen diseño figma tal, el nombre de la rama está mal
+                </div>
+            </div>
+        </section>
+    );
+}
