ef456f071ad811a6efe0bcb28bb35e8e7512fe33
KaruzG <josemiguelcpcs@gmail.com>
2025-10-13 19:34:13 +0200

    feat(useWeatherData): Hook to call API for weather data

--- hooks/useWeatherData.ts
+++ hooks/useWeatherData.ts
@@ -1,8 +1,14 @@
-export async function useWeatherData() {
+export async function useWeatherData(coords: { lat: number; lon: number }) {
     const BASE_URL = '/api/weather/';
-    const response = await fetch(BASE_URL, { cache: 'no-store' })
+    const response = await fetch(BASE_URL, {
+        cache: 'no-store', 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ location: `${coords.lat},${coords.lon}` }),
+    })
     const data = await response.json();
-    console.log('Weather data:', data);
 
     return data;
 }
