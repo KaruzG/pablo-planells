7382071500fb1c7d480231af5b660fe41a37ead0
KaruzG <josemiguelcpcs@gmail.com>
2025-10-14 17:09:08 +0200

    fix: Unused import

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,6 @@
 import { ContactSection } from "@/components/ContactSection/ContactSection";
 import Hero from "@/components/Hero";
 import Timeline from "@/components/Timeline";
-import { useWeatherData } from "@/hooks/useWeatherData";
 
 export default function Home() {
 
