51734ed90b2fee6e947f6347e9a2b03a6b683058
KaruzG <josemiguelcpcs@gmail.com>
2025-11-10 18:55:20 +0100

    feat(projectSection): Content

--- components/ProjectsSection/ProjectsHeader.tsx
+++ components/ProjectsSection/ProjectsHeader.tsx
@@ -1,10 +1,17 @@
+import { rubikMonoOne } from "@/fonts/fonts";
 import { useTranslations } from "next-intl";
 
 const ProjectsHeader = () => {
     const t = useTranslations('projects');
     
     return (
-        <h1 className="absolute top-[-8px] text-white text-3xl text-center font-extrabold w-full mt-0">{t('header1') + '·' + t('header2') + '·' + t('header3')}</h1>
+        <h1 className={`${rubikMonoOne} flex justify-between md:px-10 px-2 absolute top-[-8px] lg:top-[-11px] text-white text-2xl sm:text-5xl lg:text-7xl text-center font-extrabold w-full mt-0`}>
+            <span>{t("header1")}</span>
+            <span>·</span>
+            <span>{t("header2")}</span>
+            <span>·</span>
+            <span>{t("header3")}</span>
+        </h1>
     )
 }
 
--- components/ProjectsSection/ProjectsSection.tsx
+++ components/ProjectsSection/ProjectsSection.tsx
@@ -6,7 +6,7 @@ const ProjectsSection = () => {
     const t = useTranslations('projects');
 
     return (
-        <section className="bg-grey relative py-5">
+        <section className="bg-grey relative py-5 lg:py-15">
             <ProjectsHeader />
             <ProjectsList />
             <h2 className="text-center text-white text-l underline">{t('footer')}</h2>
