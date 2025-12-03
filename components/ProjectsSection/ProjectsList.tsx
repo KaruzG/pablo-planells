db90ca002a8f9285a85a9479e844c31ef32593af
KaruzG <josemiguelcpcs@gmail.com>
2025-12-03 18:29:16 +0100

    feat(ProjectList): Made x scroll

--- components/ProjectsSection/ProjectsList.tsx
+++ components/ProjectsSection/ProjectsList.tsx
@@ -2,10 +2,12 @@ import ProjectCard from "./ProjectCard/ProjectCard";
 
 const ProjectsList = () => {
   return (
-    <ul className="flex flex-wrap lg:gap-15 gap-5 justify-center py-10">
-      <li><ProjectCard className="bg-white rounded-3xl" imgSrc="/images/JACKET 2.png" width={300} height={300} altText="Jacket"></ProjectCard></li>
-      <li><ProjectCard className="bg-white rounded-3xl" imgSrc="/images/JACKET 1.png" width={300} height={300} altText="Jacket"></ProjectCard></li>
-      <li><ProjectCard className="bg-white rounded-3xl" imgSrc="/images/TOTE 1.png" width={300} height={300} altText="Jacket"></ProjectCard></li>
+    <ul className="flex flex-row lg:gap-15 gap-5 pl-5 md:pl-15 pr-5 justify-start py-10 overflow-x-scroll">
+      <li><ProjectCard className="bg-white rounded-xl min-w-3xs md:min-w-xs" imgSrc="/images/JACKET 2.png" width={300} height={300} altText="Jacket"></ProjectCard></li>
+      <li><ProjectCard className="bg-white rounded-xl min-w-3xs md:min-w-xs" imgSrc="/images/JACKET 2.png" width={300} height={300} altText="Jacket"></ProjectCard></li>
+      <li><ProjectCard className="bg-white rounded-xl min-w-3xs md:min-w-xs" imgSrc="/images/JACKET 2.png" width={300} height={300} altText="Jacket"></ProjectCard></li>
+      <li><ProjectCard className="bg-white rounded-xl min-w-3xs md:min-w-xs" imgSrc="/images/JACKET 2.png" width={300} height={300} altText="Jacket"></ProjectCard></li>
+      <li><ProjectCard className="bg-white rounded-xl min-w-3xs md:min-w-xs" imgSrc="/images/JACKET 2.png" width={300} height={300} altText="Jacket"></ProjectCard></li>
     </ul>
   )
 };
