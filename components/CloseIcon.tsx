7bcdd7e3660970336c18c2e08a32a0786467cd90
KaruzG <josemiguelcpcs@gmail.com>
2025-12-03 18:19:07 +0100

    feat(Cards): Added modal component for the cards

---
+++ components/CloseIcon.tsx
@@ -0,0 +1,15 @@
+const CloseIcon = () => {
+    return (
+        <svg
+        className="w-6 h-6 cursor-pointer"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        >
+            <path d="M6 18L18 6M6 6l12 12"></path>
+        </svg>
+    )}
+export default CloseIcon;
