9010ffba718e2934c78a66ecc8fcb455e299621d
KaruzG <josemiguelcpcs@gmail.com>
2025-12-08 22:47:14 +0100

    chore!: Upgrade next to 16v

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
     transpilePackages: ['three'],
+    experimental: {
+        globalNotFound: true,
+    },
 };
 
 export default withNextIntl(nextConfig);
