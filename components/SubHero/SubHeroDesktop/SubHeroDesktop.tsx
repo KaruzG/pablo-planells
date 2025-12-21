96951bfaf86e3537518f4850cf5e44249a310516
KaruzG <josemiguelcpcs@gmail.com>
2025-12-21 14:22:52 +0100

    sync

--- components/SubHero/SubHeroDesktop/SubHeroDesktop.tsx
+++ components/SubHero/SubHeroDesktop/SubHeroDesktop.tsx
@@ -1,7 +1,11 @@
+import SubHeroDesktopText from "./SubHeroDesktopText";
+import SubHeroDesktopGrid from "./SubHeroDestopGrid";
+
 const SubHeroDesktop = () => {
     return (
         <section>
-
+            <SubHeroDesktopGrid></SubHeroDesktopGrid>
+            <SubHeroDesktopText/>
         </section>
     )
 }
---
+++ components/SubHero/SubHeroDesktop/SubHeroDestopGrid.tsx
@@ -0,0 +1,13 @@
+interface Props {
+    className?: string;
+}
+
+const SubHeroDesktopGrid = ( params:Props ) => {
+    return (
+        <div className={params.className}>
+            
+        </div>
+    )
+}
+
+export default SubHeroDesktopGrid;
