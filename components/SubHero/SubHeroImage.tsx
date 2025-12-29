477d4c24d6f026988b2dc5ebaf4c455631e6deeb
KaruzG <josemiguelcpcs@gmail.com>
2025-12-29 14:06:27 +0100

    feat(subHero): New Image

--- components/SubHero/SubHeroImage.tsx
+++ components/SubHero/SubHeroImage.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import pabloSubHero from "@public/images/pabloSubHero.png";
 
 const SubHeroImage = () => {
     return (
         <div className="w-96 md:w-[500px] mb-6">
-            <Image layout="responsive" src="/images/subHeroPablo.png" height={300} width={300} alt="Photo of Pablo Sewing"></Image>
+            <Image className="rounded-2xl" layout="responsive" src={pabloSubHero} height={300} width={300} alt="Photo of Pablo Sewing"></Image>
         </div>
     )
 }
