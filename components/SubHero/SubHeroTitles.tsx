db3a2060f0b5861ccb0d2c15e95005f9cdbe2af4
KaruzG <josemiguelcpcs@gmail.com>
2026-01-16 19:03:30 +0100

    fix(animations): Only one trigger

--- components/SubHero/SubHeroTitles.tsx
+++ components/SubHero/SubHeroTitles.tsx
@@ -9,13 +9,13 @@ import { springRight } from "@/animations/onViewAnimations";
 const SubHeroTitles = () => {
     const t = useTranslations('subhero')
     return (
-        <motion.div className="lg:max-w-xl xl:max-w-2xl">
-            <motion.div {...springRight}>
+        <div className="lg:max-w-xl xl:max-w-2xl">
+            <motion.div viewport={{ once: true }} {...springRight}>
                 <h1 className={`${rubikMonoOne.className} text-4xl text-center tracking-tighter md:text-6xl lg:text-right xl:text-8xl`}>{t("header")}</h1>
                 <h2 className={`${rubik.className} text-2xl text-center tracking-tight md:text-4xl lg:text-right mb-6`}>{t("header2")}</h2>
             </motion.div>
             <SocialMedia animation={true} className="lg:justify-end gap-6 justify-center" size="lg" />
-        </motion.div>
+        </div>
     )
 }
 export default SubHeroTitles;
